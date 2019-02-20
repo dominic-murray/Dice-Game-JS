fe19488e56576ba37df0ba2e122df4312f10f9e0
dominic-murray <dominic-murray@hotmail.co.uk>
2019-02-20 15:56:20 +0000

    added another dice, change some rules and added a final score section

--- Desktop/Desktop/Javascript-course-PT1/4-DOM-pig-game/starter/app.js
+++ Desktop/Desktop/Javascript-course-PT1/4-DOM-pig-game/starter/app.js
@@ -13,14 +13,18 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
+var lastDice;
+
 // initilaize function to restart game
 
 function init() {
     scores = [0,0];
     activePlayer = 0;
     roundScore = 0;
+    gamePlaying = true;
     
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -37,39 +41,68 @@ function init() {
 };
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    if(gamePlaying) {
+    if (gamePlaying) {
     // Random Number
-   var dice = Math.floor(Math.random() * 6) + 1;
+   var dice1 = Math.floor(Math.random() * 6) + 1;
+   var dice2 = Math.floor(Math.random() * 6) + 1;
     
     // display result
-   var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+   document.getElementById('dice-1').style.display = 'block';
+   document.getElementById('dice-2').style.display = 'block';
+   document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+   document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+     
+    if (dice1 !== 1 && dice2 !== 1) {
+        //add score
+        roundScore += dice1 + dice2;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+       nextPlayer();
+    }     
         
     // update the round score IF rolled number is not a 1
-    if (dice !== 1) {
+    /*
+    if (dice === 6 && lastDice === 6) {
+       // player losses score when rolling two 6's 
+        scores[activePlayer] = 0;
+        document.querySelector('#score-' + activePlayer).textContent = '0';
+        nextPlayer();
+    } else if (dice !== 1) {
         //add score
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        // next player
        nextPlayer();
-    }  
+    } 
+        lastDice = dice;
+        */
  }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  if(gamePlaying) {
+  if (gamePlaying) {
     // Add current score to global score
     scores[activePlayer] += roundScore;
     
     // update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
+    // adding final score 
+    var input = document.querySelector('.final-score').value; 
+     var winningScore = input;
+    // undefind, 0, null or "" are coerced to false
+    // anything else is coerced to true
+    if(input) {
+        winningScore = input;
+    } else {
+        winningScore = 100;
+    }
+      
     // check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         gamePlaying = false;
@@ -80,20 +113,23 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 }); 
 
     // Next player function
-    function nextPlayer() {
+function nextPlayer() {
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
-       roundScore = 0;
+        roundScore = 0;
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
     };
 
 // new game button
 
 document.querySelector('.btn-new').addEventListener('click', init); 
     
+
+// some rules player losses score when rolling two 6's
 
 
 
